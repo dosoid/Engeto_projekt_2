b31497092f55c1ea396dbe6f54291720991cacce
dosoid <dos333@seznam.cz>
2025-02-01 12:43:48 +0100

    Doplněná funkce na kontrolu vstupu uživatele, počitání bullCow a tisk výsledku

--- Projekt_2.py
+++ Projekt_2.py
@@ -5,8 +5,10 @@ projekt_2.py: druhý projekt do Engeto Online Python Akademie
 author: Evžen Oskin
 email: dos333@seznam.cz
 """
-import random
+
 import projekt_2_funkce as p2f
+
+nahodne_cislo = p2f.nahodne_cislo() # vytvoří list 4 čísel
 
 print("Ahoj! Vítej ve hře Bulls & Cows!")
 p2f.rozdelovnik()
@@ -14,9 +16,23 @@ print("Vytvořil jsem náhodné čtyřmistné číslo. Tvým úkolem je ho uhodn
       "Pojďme hrát!",
       sep="\n")
 p2f.rozdelovnik()
-print("Zadej čtyřmistné číslo:")
+cislo = p2f.vstup() # vstup od uživatele
 p2f.rozdelovnik()
 
-nahodne_cislo = p2f.nahodne_cislo() # vysledek je list 4 čísel
-cislo = input() # vstup od uživatele
+cislo_list = [int(cislo[i] for i in range(4))] # převede vstup od uživatele na list
 
+bull_cow = p2f.porovnej(cislo = cislo_list, nahodne_cislo = nahodne_cislo)
+
+while bull_cow[0] != 4:
+      if bull_cow[0] == 1 or bull_cow[1] == 1:
+            print(f"{bull_cow[0]} Bull, {bull_cow[1]} Cow")
+      else:
+            print(f"{bull_cow[0]} Bulls, {bull_cow[1]} Cows")
+      print("Zkus to znovu:")
+      p2f.rozdelovnik()
+      cislo = p2f.vstup() 
+else:
+      print("Správně, uhodl jsi číslo!")
+      p2f.rozdelovnik()
+      print("Gratuluji!")
+      
--- projekt_2_funkce.py
+++ projekt_2_funkce.py
@@ -17,8 +17,27 @@ def nahodne_cislo() -> list:
 
     return cislo
 
+def kontrola_vstupu(cislo: str) -> bool:
+    '''Kontroluje jestli uživatel zadal správně 4-mistné číslo'''
+
+    if len(cislo) != 4 or not cislo.isgidit() or cislo[0] == "0":
+        return False
+    return True
+
+def vstup() -> str:
+    '''Vratí vstup od uživatele'''
+
+    cislo = input("Zadej čtyřmistné číslo bez 0 na začátku: ")
+    
+    while not kontrola_vstupu(cislo):
+      print("Tvůj vstup neodpovídá pravidlům, zadej znovu: ")
+      cislo = input() # vstup od uživatele
+    
+    return cislo
+
 def porovnej(cislo: list, nahodne_cislo: list) -> tuple:
     '''Porovnává čísla a vrací počet Bull a Cow'''
+
     bull = 0
     cow = 0
 
